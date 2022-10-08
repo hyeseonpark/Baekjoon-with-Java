1055f586b398459cd7f8d35191cd4fc2d70f6167
Hyeseon <68687428+hyeseonpark@users.noreply.github.com>
2022-10-08 21:24:47 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,9 +1,11 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int N = new Scanner(System.in).nextInt();
+        int N = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
 
         N= N-1;
         int cnt = 1;
