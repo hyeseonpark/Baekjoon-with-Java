0014bf99c84687a49ac455a571e0a4426a7692e0
Hyeseon <68687428+hyeseonpark@users.noreply.github.com>
2022-10-09 14:16:39 +0900

    [Bronze I] Title: 분수찾기, Time: 152 ms, Memory: 16056 KB -BaekjoonHub

--- 백준/Bronze/1193. 분수찾기/분수찾기.java
+++ 백준/Bronze/1193. 분수찾기/분수찾기.java
@@ -7,36 +7,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int X = Integer.parseInt(br.readLine());
 
-        int other = 0;
-        int time = 1;
+        int cross_cnt = 1; // 해당 대각선의 칸 개수
+        int prev_cnt_sum = 0; // 직전 대각선 칸들의 합
 
-        while(true) {
-            if ( X - forValue(time) > 0 ) {
-                time++;
-            } else {
-                other = X - forValue(time - 1);
+        while (true) {
+            //  해당 대각선의 칸 개수 + 직전 대각선 칸들의 합 이용한 범위 판별
+            if (X <= cross_cnt + prev_cnt_sum) {
+
+                if (cross_cnt % 2 == 1) { // 대각선의 칸 개수가 홀수라면
+                    // T = cross_cnt + 1
+                    // 분자는 큰 수부터 시작
+                    // 분모는 작은 수부터 시작
+                    System.out.print(
+                            (cross_cnt - (X - prev_cnt_sum - 1))
+                            + "/"
+                            + (X - prev_cnt_sum));
+
+                } else { // 대각선의 개수가 짝수라면
+                    System.out.print(
+                            (X - prev_cnt_sum)
+                                    + "/"
+                                    + (cross_cnt - (X - prev_cnt_sum - 1)));
+                }
                 break;
+            } else {
+                prev_cnt_sum += cross_cnt;
+                cross_cnt++;
             }
         }
-
-        int[] arr = new int[time];
-        for (int i=0; i < time; i++) {
-            arr[i] = i + 1;
-        }
-
-        if (time % 2 == 0) { // 짝수
-            System.out.println(arr[other -1] + "/" + arr[arr.length - other]);
-        } else {
-            System.out.println(arr[arr.length - other] + "/" + arr[other -1]);
-        }
     }
 
-    public static int forValue(int value) {
-        int returnValue = 0;
-        for (int i=value; i > 0 ; i--) {
-            returnValue += i;
-        }
-
-        return returnValue;
-    }
 }
