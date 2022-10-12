c8d81cfdb6637878bb9ceb15eedf8c44c8e609fc
Hyeseon <68687428+hyeseonpark@users.noreply.github.com>
2022-10-12 22:15:10 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        char[] arr = new BufferedReader(new InputStreamReader(System.in)).readLine().toCharArray();
+        Arrays.sort(arr);
+
+        for (int i=arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+     }
+}
